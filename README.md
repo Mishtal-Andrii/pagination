229a8c6649b85cac791b34952930e843afa9f358
Andrii Mishtal <mishtal.andriy@gmail.com>
2021-02-18 18:02:35 +0200

    start

---
+++ README.md
@@ -0,0 +1,2 @@
+# pagination
+[DEMO LINK]()
---
+++ app.js
@@ -0,0 +1,7 @@
+import fetchFollowers from './fetchFollowers.js'
+import displayFollowers from './displayFollowers.js'
+import paginate from './paginate.js'
+import displayButtons from './displayButtons.js'
+
+const title = document.querySelector('.section-title h1')
+const btnContainer = document.querySelector('.btn-container')
